29d1d77b00d4835499c00a04c61c5f3744b6355e
Daniel Maninger <daniel.maninger@stud.tu-darmstadt.de>
2022-09-07 13:11:33 +0200

    Add example of a bug in scatterUpdate

---
+++ src/main/java/ScatterUpdate.java
@@ -0,0 +1,12 @@
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.factory.ops.NDBase;
+
+public class ScatterUpdate {
+
+    public static void main(String[] args) {
+        INDArray v1 = Nd4j.zeros(5);
+        INDArray v2 = new NDBase().scatterUpdate(v1, Nd4j.scalar(2), Nd4j.scalar(1f));
+        System.out.println(v2); // expected result: [0, 0, 1, 0, 0]
+    }
+}
