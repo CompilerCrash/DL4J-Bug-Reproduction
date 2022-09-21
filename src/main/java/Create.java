926c4fd8a11992fe6f0c4ea99769783f3c185740
Daniel Maninger <daniel.maninger@stud.tu-darmstadt.de>
2022-09-21 15:24:10 +0200

    Add example for bug in create

---
+++ src/main/java/Create.java
@@ -0,0 +1,13 @@
+import org.nd4j.autodiff.samediff.SDVariable;
+import org.nd4j.autodiff.samediff.SameDiff;
+import org.nd4j.linalg.api.buffer.DataType;
+
+public class Create {
+
+    public static void main(String[] args) {
+        SameDiff sd = SameDiff.create();
+        SDVariable var = sd.create(null, sd.constant(8), DataType.INT32);
+        System.out.println(var.dataType()); // should be INT32 but is FLOAT
+        System.out.println(var.eval()); // should be uninitialized but is (seemingly) zero-initialized
+    }
+}
