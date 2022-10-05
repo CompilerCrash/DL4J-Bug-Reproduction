ab12c22ce7ec6a4a5b08ed2fd1c62f180b184b16
Daniel Maninger <daniel.maninger@stud.tu-darmstadt.de>
2022-10-05 15:59:58 +0200

    Add example for shape op on non-numerical type

--- src/main/java/Create.java
+++ src/main/java/Create.java
@@ -9,5 +9,9 @@ public class Create {
         SDVariable var = sd.create(null, sd.constant(8), DataType.INT32);
         System.out.println(var.dataType()); // should be INT32 but is FLOAT
         System.out.println(var.eval()); // should be uninitialized but is (seemingly) zero-initialized
+
+        System.out.println(var.shape().eval()); // [8]
+        var = var.castTo(DataType.BOOL);
+        System.out.println(var.shape().eval()); // throws exception (shape op requires numerical type)
     }
 }
