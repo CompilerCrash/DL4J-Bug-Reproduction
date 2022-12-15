237fd5d3fc0e418dbb8a1ef555225a550426922a
Daniel Maninger <daniel.maninger@tu-darmstadt.de>
2022-12-15 19:36:00 +0100

    Add example for bug in expandDims

--- src/main/java/ExpandDims.java
+++ src/main/java/ExpandDims.java
@@ -4,12 +4,16 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.exception.ND4JIllegalStateException;
 import org.nd4j.linalg.factory.Nd4j;
 
+import java.util.Arrays;
+
 public class ExpandDims {
 
     public static void main(String[] args) {
         String separator = "=".repeat(40);
 
         expand2dTo3d();
+        System.out.println(separator);
+        expandLastAxisOfScalar();
         System.out.println(separator);
         expandLastAxis();
         System.out.println(separator);
@@ -18,25 +22,32 @@ public class ExpandDims {
         sdExpandOutOfBounds();
     }
 
-    // FIXME
     public static void expand2dTo3d() {
         SameDiff sd = SameDiff.create();
         SDVariable v1 = sd.zero(null, 1, 1);
-        SDVariable v2 = sd.expandDims(v1, -1); // new: validation error
+        SDVariable v2 = sd.expandDims(v1, 2);
         System.out.println(v1.shape().eval()); // [1, 1]
         System.out.println(v2.shape().eval()); // should be [1, 1, 1] but is [1, 1]
     }
 
-    public static void expandLastAxis() {
-        INDArray v1 = Nd4j.scalar(0);
-        System.out.println(java.util.Arrays.toString(v1.shape())); // []
+    public static void expandLastAxisOfScalar() {
+        INDArray v1 = Nd4j.scalar(1);
+        System.out.println(Arrays.toString(v1.shape())); // []
         INDArray v2 = Nd4j.expandDims(v1, -1); // throws exception
-        System.out.println(java.util.Arrays.toString(v2.shape())); // should now be [1]
+        System.out.println(Arrays.toString(v2.shape())); // should now be [1]
+    }
+
+    // FIXME
+    public static void expandLastAxis() {
+        INDArray v1 = Nd4j.ones(2, 2);
+        System.out.println(Arrays.toString(v1.shape())); // [2, 2]
+        INDArray v2 = Nd4j.expandDims(v1, 2); // throws exception
+        System.out.println(Arrays.toString(v2.shape())); // should now be [2, 2, 1]
     }
 
     public static void nd4jExpandOutOfBounds() {
         try {
-            INDArray v1 = Nd4j.zeros(1, 1);
+            INDArray v1 = Nd4j.ones(2, 2);
             INDArray v2 = Nd4j.expandDims(v1, 3); // crashes
         } catch (ND4JIllegalStateException e) {
             e.printStackTrace();
@@ -46,7 +57,7 @@ public class ExpandDims {
     public static void sdExpandOutOfBounds() {
         try {
             SameDiff sd = SameDiff.create();
-            SDVariable v1 = sd.zero(null, 1, 1);
+            SDVariable v1 = sd.one(null, 2, 2);
             SDVariable v2 = sd.expandDims(v1, 3);
             v2.shape().eval(); // crashes
         } catch (ND4JIllegalStateException e) {
